6f8a2d625a43643255b4dee5bebd758faac9bfb5
Luis Gaona <luis10828steam@gmail.com>
2024-02-12 13:11:40 -0500

    Hola

---
+++ shopping-service/src/main/java/com/example/demo/client/CustomerClient.java
@@ -0,0 +1,14 @@
+package com.example.demo.client;
+
+import com.example.demo.model.Customer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@Fei(name = "customer-service", fallback = CustomerHystrixFallbackFactory.class)
+//@RequestMapping(value = "/products")
+//public interface  CustomerClient {
+//    @GetMapping(value = "/customers/{id}")
+//    public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
+//}
