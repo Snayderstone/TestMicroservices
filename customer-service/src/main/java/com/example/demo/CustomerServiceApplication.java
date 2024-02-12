4b5e9fd165cdcea458b93cb1d9a019407375b6df
Luis Gaona <luis10828steam@gmail.com>
2024-02-12 18:04:46 -0500

    si

--- customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CustomerServiceApplication {
 
